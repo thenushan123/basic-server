797262993ae8c6785b7f238514a3bceead6856ff
thenushan <thenushan.sivanesan@gmail.com>
2024-07-23 10:19:12 +0100

    Added cors for cross origin communication

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const app = express()
-const fruitRouter = require('./routes/fruitRouter')
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const fruitRouter = require("./routes/fruitRouter");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.use(express.json())
-app.use('/fruits', fruitRouter)
+// CORS
+app.use(cors());
+app.use(express.json());
+app.use("/fruits", fruitRouter);
 
-
-module.exports = app
+module.exports = app;
